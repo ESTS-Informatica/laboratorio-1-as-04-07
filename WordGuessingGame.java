bffbb714a568b7d7afd4452d00423f489b9d9915
AntonioGuerreiro <202200160@estudantes.ips.pt>
2024-03-13 10:26:58 +0000

    nível 2

--- WordGuessingGame.java
+++ WordGuessingGame.java
@@ -10,26 +10,58 @@ public class WordGuessingGame
     private String hiddenWord;
     private String guessedWord;
     private int numberOfTries;
-    
-    public WordGuessingGame(){
+    private InputReader reader;
+
+    public WordGuessingGame(InputReader reader){
         hiddenWord = "abc";
         guessedWord = "___";
         numberOfTries = 0;
+        this.reader = reader;
     }
-    
+
     public String getHiddenWord(){
         return hiddenWord;
     }
-    
+
     public String getGuessedWord(){
         return guessedWord;
     }
-    
+
     public int getNumberOfTries(){
         return numberOfTries;
     }
-    
-    public void showGuessedWord(){
-        System.out.println(getGuessedWord());
+
+    public void play(char c){
+        showWelcome();
+        guess(c);
+        if(guessedWord == hiddenWord){
+            showResult();
+        }
+    }
+
+    private void showGuessedWord(){
+        System.out.println(reader.getChar(getGuessedWord()));
+    }
+
+    private void showWelcome(){
+        System.out.println("*** Bem-vindo(a) ao Jogo! ***");
+    }
+
+    private void guess(char c){
+        char guessedChar = reader.getChar(getGuessedWord());
+        char hiddenChar = reader.getChar(getHiddenWord());
+        
+        for(int i=0; i < hiddenWord.length(); i++){
+            if(guessedChar == hiddenWord.charAt(i)){
+                guessedChar = hiddenChar;
+            }
+        }
+    }
+
+    private void showResult(){
+        if(getHiddenWord() == getGuessedWord()){
+            showGuessedWord();
+            System.out.println("Número de tentativas: " + getNumberOfTries());
+        }
     }
 }
